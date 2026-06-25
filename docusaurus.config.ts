8c8a529db4cc74ea2a1e8a18f181eb3fb4fa3b79
Joost Godschalk <j.c.godschalk@codeglass.io>
2026-06-25 15:14:59 +0200

    Fixed version mismatch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'http://localhost:30340', 
   // Set the /<baseUrl>/ pathname under which your site is served (Make sure that it ends with '/', and that the name is not'docs' as it does not like that)
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/documentation/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
